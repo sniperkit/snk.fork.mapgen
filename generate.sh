5b5a7d04d9a3b2e40b2c3f62badb987656936a75
Ammar Bandukwala <ammar@ammar.io>
2017-08-02 14:02:59 -0500

    fix pacakge in example

--- generate.sh
+++ generate.sh
@@ -3,5 +3,5 @@ rm example/channel_int_map_gen.go
 go-bindata -pkg mapgen map.go.tmpl
 go install github.com/ammario/mapgen/cmd/mapgen 
 go generate
-mapgen -f example/channel_int_map_gen.go Channel/int
+mapgen -p example -f example/channel_int_map_gen.go Channel/int
 
